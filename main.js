839c18e367436c8b4e88a74a1730aec74a9b3bd1
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-07-30 17:57:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 const form = document.querySelector('#form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    
+});
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+};
